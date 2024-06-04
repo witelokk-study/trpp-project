87bd18516256fd545bd1f478da50e8dda218f851
benzlokzik <121052717+benzlokzik@users.noreply.github.com>
2024-06-04 08:30:56 +0300

    add DefaultStyle

---
+++ services/frontend/todo-front/src/components/DefaultStyle.svelte
@@ -0,0 +1,27 @@
+<style>
+    :root {
+        font-family: Arial, sans-serif;
+        margin: 20px;
+        background-color: #0b0b1d;
+        color: #d7c7e4;
+    }
+
+    nav {
+        display: flex;
+        justify-content: space-around;
+        background-color: #241a36;
+        padding: 10px;
+        margin-bottom: 20px;
+    }
+
+    nav a {
+        color: #d7c7e4;
+        text-decoration: none;
+        padding: 10px;
+    }
+
+    nav a:hover {
+        background-color: #855264;
+        border-radius: 5px;
+    }
+</style>
