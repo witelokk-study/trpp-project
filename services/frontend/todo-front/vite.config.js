5b5bf4b8855387773300fd5a3b683b2942e24b81
benzlokzik <121052717+benzlokzik@users.noreply.github.com>
2024-06-04 10:51:11 +0300

    chore: Update environment variable handling in vite.config.js

--- services/frontend/todo-front/vite.config.js
+++ services/frontend/todo-front/vite.config.js
@@ -1,6 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-	plugins: [sveltekit()]
-});
+
+// Load environment variables based on the current mode
+export default ({ mode }) => {
+	const env = loadEnv(mode, process.cwd());
+
+	return defineConfig({
+		plugins: [sveltekit()],
+		define: {
+			'process.env.API_URL': JSON.stringify(env.API_URL)
+		},
+	});
+};
